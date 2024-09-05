1f5d757bd23bed3d062c5d93a302ceaea989f8c3
Leonard Brebene <74564605+leonardBrebene@users.noreply.github.com>
2024-09-05 11:19:45 +0300

    Deployed in Google Cloud

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require( 'express');
 const app = express ();
 app.use (express.json());
-app.use (express.static( 'react-app/dist'));
+app.use (express.static('simple-react-app/build'));
 const port = process.env.PORT || 8080;
 app. listen(port, () => {
 console. log(`Listening on port ${port}`);
