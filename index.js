3f7601a8667c18e69663596b00937892c0997bad
Leonard Brebene <74564605+leonardBrebene@users.noreply.github.com>
2024-09-05 10:37:47 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require( 'express');
+const app = express ();
+app.use (express.json());
+app.use (express.static( 'react-app/dist'));
+const port = process.env.PORT || 8080;
+app. listen(port, () => {
+console. log(`Listening on port ${port}`);
+});
+
+app.get ('/pirates', (req, res) => {
+    res.send({ data: "I am a pirate" });
+})
+   
